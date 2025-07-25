e39fbbf8e17448c9162e553bbf1de7489b150e7a
benedekaibas <kaibas.benedek02@gmail.com>
2025-07-25 20:24:18 +0200

    friend class structure generated for analyzing

---
+++ src/build/CMakeFiles/analyze-tool.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/analyze-tool.dir/computer-science/detect_unused_var.cpp.o"
+  "CMakeFiles/analyze-tool.dir/computer-science/detect_unused_var.cpp.o.d"
+  "analyze-tool"
+  "analyze-tool.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/analyze-tool.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/computer-science/detect_unused_var.cpp
+++ src/computer-science/detect_unused_var.cpp
@@ -1,3 +1,29 @@
 #include <iostream>
 #include "clang/AST/DeclCXX.h"
 
+class ObjectClass {
+private:
+  int privateData;
+protected:
+  int protectedData;
+public:
+    ObjectClass() : privateData(), protectedData() {}
+
+    friend class FriendClass;
+};
+
+class FriendClass {
+public:
+  void getAccessToObjectClass(ObjectClass& obj){
+    obj.privateData = 100;
+    obj.protectedData = 200;
+
+    std::cout << obj.privateData;
+  }
+
+};
+
+int main(){
+ FriendClass obj;
+ obj.getAccessToObjectClass();
+}
